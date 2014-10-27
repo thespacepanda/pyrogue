f01e69c041a1f658a678b75ed9a13a3b9539c61f
robopino <boyd.robertj@gmail.com>
2014-10-27 14:39:10 -0400

    Initial libcotd setup, just clone this

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello")
+import libtcodpy as libtcod
+
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
+
+libtcod.console_set_custom_font(b'arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, b'python/libtcod tutorial', False)
+libtcod.sys_set_fps(LIMIT_FPS)
+
+while not libtcod.console_is_window_closed():
+	libtcod.console_set_default_foreground(0, libtcod.white)
+	libtcod.console_put_char(0, 1, 1, b'@', libtcod.BKGND_NONE)
+	libtcod.console_flush()

ee39a77f403bd716afb2ae973959acb9b6868152
Britt Mathis <britt.mathis@gmail.com>
2014-10-29 14:54:03 -0400

    refactored the rendering logic to graphics.py

--- graphics.py
+++ graphics.py
@@ -1,16 +1,26 @@
 import libtcodpy as libtcod
 
-class Show():
+class Stamp:
     """
     This class represents drawing the game to a given virtual console,
     which is blitted to the real console in main. You initialize this
-    class with a virtual console and an optional list of entities to
-    draw. This class assumes that the entities have a character, a
-    color, and x, y coordinates.
+    class with a virtual console, and an optional entity list. This
+    class assumes that the entities have a character, a color, and
+    x, y coordinates.
     """
-    def __init__(self, console, entities=[]):
-        self.console = console
+    def __init__(self, entities=[], SCREEN_WIDTH=80, SCREEN_HEIGHT=50):
         self.entities = entities
+        self.SCREEN_WIDTH = SCREEN_WIDTH
+        self.SCREEN_HEIGHT = SCREEN_HEIGHT
+        self.console = libtcod.console_new(self.SCREEN_WIDTH,
+                                           self.SCREEN_HEIGHT)         
+        libtcod.console_set_custom_font(b'arial10x10.png',
+                                        libtcod.FONT_TYPE_GREYSCALE |
+                                        libtcod.FONT_LAYOUT_TCOD)
+        libtcod.console_init_root(self.SCREEN_WIDTH, self.SCREEN_HEIGHT,
+                                  b'python/libtcod tutorial', False)
+
+        
     def draw(self):
         """
         Iterates through self.entities and draws each to the virtual
@@ -22,6 +32,10 @@ class Show():
                                                    entity.color)
             libtcod.console_put_char(self.console, entity.x, entity.y,
                                      entity.char, libtcod.BKGND_NONE)
+        self.blit()
+        self.flush()
+        #self.clear()
+        
     def clear(self):
         """
         Iterates through self.entities and clears the position they
@@ -33,6 +47,12 @@ class Show():
         for entity in self.entities:
             libtcod.console_put_char(self.console, entity.x, entity.y,
                                     b' ', libtcod.BKGND_NONE)
+        
+    def blit(self):
+        libtcod.console_blit(self.console, 0, 0, self.SCREEN_WIDTH,
+                             self.SCREEN_HEIGHT, 0, 0, 0)
+    def flush(self):
+        libtcod.console_flush()
     def addEntity(self, entity):
         """
         Adds a new entity to the internal list of entities to draw.
--- test.py
+++ test.py
@@ -1,45 +1,31 @@
 import libtcodpy as libtcod
 
 from entity import Entity
-from graphics import Show
+from graphics import Stamp
 from keys import handleKeys
 
 #############################################
 # Initialization & Main Loop
 #############################################
- 
-# Global screen size
-SCREEN_WIDTH  = 80
-SCREEN_HEIGHT = 50
 
-libtcod.console_set_custom_font(b'arial10x10.png',
-                                libtcod.FONT_TYPE_GREYSCALE |
-                                libtcod.FONT_LAYOUT_TCOD)
-libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT,
-                          b'python/libtcod tutorial', False)
-
-# Make an off screen console that we can draw to, we will blit it on
-# to the actual console in main
-con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
-
-player = Entity(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, b'@', libtcod.white, con)
-npc = Entity(SCREEN_WIDTH//2 + 3, SCREEN_HEIGHT//2, b'@', libtcod.yellow, con)
+player = Entity(80//2, 50//2, b'@', libtcod.white)
+npc = Entity(80//2 + 3, 50//2, b'@', libtcod.yellow)
 
 entities = [player, npc]
 
-show = Show(con, entities)
+stamp = Stamp(entities)
 
 while not libtcod.console_is_window_closed():
 
-    show.draw()
+    stamp.draw()
     
     # Since we are drawing to an off-screen console, we need to "blit"
     # it to the actual console - this just means we copy it over
-    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
+    #libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
     # This flushes the changes in the console to the screen itself
-    libtcod.console_flush()
+    #libtcod.console_flush()
     
-    show.clear()
+    stamp.clear()
     
     # Handle keys and exit game if needed
     exit = handleKeys(player)

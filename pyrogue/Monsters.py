9b6b24f912d4ca7f62e0078d7b7f718366915b63
Nightstick <stephenlangenkamp04@flpoly.org>
2014-12-01 11:27:12 -0500

    Revert "Monster Class"
    This reverts commit 3dfefe9d524109124c7e1ec8f60e69892b96dd0c.
    
    Monster Class
    
    I think this creates classes for various monsters

---
+++ pyrogue/Monsters.py
@@ -0,0 +1,51 @@
+"""
+    pyrogue.weapon
+    ~~~~~~~~~~~~
+
+    This module represents drawable objects which are not static - they can be interacted with.
+"""
+
+import colors
+
+class Monsters(Entity):
+    """This takes a character, a color, and a tuple as the map position"""
+    def __init__(self, obstacle, opaque=None):
+        self.obstacle = obstacle
+        if opaque is None:
+            opaque = obstacle
+        self.opaque = opaque
+
+class SteamElephant(Monsters):
+    """A Steam powered elephant used for hauling cargo"""
+    def __init__(self):
+        super().__init__(True)
+        self.character = b'M;'
+        self.color = colors.YELLOW
+
+class DrillSnake(Monster):
+    """A mechanical snake with a drill head used for tunneling"""
+    def __init__(self):
+        super().__init__(False)
+        self.character = b'~~:>'
+        self.color = colors.GREEN
+
+class Airship(Monster):
+    """A Flying Machine used for security"""
+    def __init__(self):
+        super().__init__(False)
+        self.character = b')!('
+        self.color = colors.RED
+
+class Locomonster(Monster):
+    """A Train monster used for transportation"""
+    def __init__(self):
+        super().__init__(False)
+        self.character = b'o=o=o=b'
+        self.color = colors.RED
+
+class robug(Monster):
+    """A giant mechanincal bug used for...I have no idea."""
+    def __init__(self):
+        super().__init__(False)
+        self.character = b'mmmm"'
+        self.color = colors.GREEN

8335a84373747ab68ae3b34d7e539880b502658f
Nightstick <stephenlangenkamp04@flpoly.org>
2014-12-01 16:59:18 -0500

    Creates Jewelry Classes
    This creates some jewelry such as common gems, but also has parts that
    may be useful in a steam punk society.

---
+++ pyrogue/Jewelry.py
@@ -0,0 +1,40 @@
+"""
+    pyrogue.Jewelry
+    ~~~~~~~~~~~~
+
+    This module represents drawable objects which are not static - they can be interacted with.
+"""
+
+import colors
+
+class Jewelry(Entity):
+    """This takes a character, a color, and a tuple as the map position"""
+    def __init__(self, obstacle, opaque=None):
+        self.obstacle = obstacle
+        if opaque is None:
+            opaque = obstacle
+        self.opaque = opaque
+
+class Gear(Jewelry):
+    """A gear from a robot"""
+    def __init__(self):
+        self.character = b'*'
+        self.color = colors.YELLOW
+
+class Diamond(Jewelry):
+    """A Diamond"""
+    def __init__(self):
+        self.character = b'<>'
+        self.color = colors.RED
+
+class Pearl(Jewelry):
+    """A Pearl"""
+    def __init__(self):
+        self.character = b'O'
+        self.color = colors.WHITE
+
+class Wire(Jewelry):
+    """Wire from a robot"""
+    def __init__(self):
+        self.character = b'&'
+        self.color = colors.RED

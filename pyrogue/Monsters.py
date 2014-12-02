f9a693d4cab2da7d419364d442404746090b0097
Britt Mathis <britt.mathis@gmail.com>
2014-12-02 15:47:50 -0500

    Characters for entities must be a single character

--- pyrogue/Monsters.py
+++ pyrogue/Monsters.py
@@ -9,38 +9,37 @@ import colors
 
 class Monsters(Entity):
     """This takes a character, a color, and a tuple as the map position"""
-    def __init__(self, obstacle, opaque=None):
-        self.obstacle = obstacle
-            opaque = obstacle
-        self.opaque = opaque
+    def __init__(self):
+        self.obstacle = True
+        self.opaque = True
 
 class SteamElephant(Monsters):
     """A Steam powered elephant used for hauling cargo"""
     def __init__(self):
-        self.character = b'M;'
+        self.character = b'E'
         self.color = colors.WHITE
 
 class DrillSnake(Monster):
     """A mechanical snake with a drill head used for tunneling"""
     def __init__(self):
-        self.character = b'~~:>'
+        self.character = b'S'
         self.color = colors.GREEN
 
 class Airship(Monster):
     """A Flying Machine used for security"""
     def __init__(self):
-        self.character = b'><'
+        self.character = b'A'
         self.color = colors.RED
 
 class Locomonster(Monster):
     """A Train monster used for transportation"""
     def __init__(self):
-        self.character = b'o=o=o=k'
+        self.character = b'L'
         self.color = colors.YELLOW
 
 class robug(Monster):
     """A giant mechanincal bug used for...I have no idea."""
     def __init__(self):
-        self.character = b'mmmm"'
+        self.character = b'R'
         self.color = colors.GREEN
 
